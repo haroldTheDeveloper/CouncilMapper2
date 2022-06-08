6fbc9a726570ea24d354cbe6fa8cc74205acf50a
haroldTheDeveloper <samuelb0801@gmail.com>
2022-06-08 17:19:49 +0100

    IDK

--- scripts/mapEvents.js
+++ scripts/mapEvents.js
@@ -1,16 +1,36 @@
-const assign = async function(District_ID, Constituency){
+const assign = function(District_ID, Constituency_ID, populations){
+  for (let i = 0; i < populations.length; i++){
+    if (District_ID == populations[i].ID){
+      populations[i].Constituency = Constituency_ID
+      console.log(populations[i].Constituency)
+      return populations
+      break
+    }
+  }
 }
 
-const populations_Parse = async function(file){
-  var data = $.csv.toObjects(file);
-  return data
-}
-
-const population = function(populations, District_ID){
+const populationDis = function(populations, District_ID){
   for (let i = 0; i < populations.length; i++){
     if (District_ID == populations[i].ID){
       return populations[i].AllAges;
       break
     }
   }
+}
+
+const colourSeat = function(layer, Constituency_ID, colours){
+  colour = colours[Constituency_ID]
+  layer.setStyle({fillColor:colour, weight: 0})
+}
+
+const populationCalc = function(Constituency_ID, populations, populations2){
+  populations2[0] = 0
+  for (let i = 0; i < populations.length; i++){
+    if (populations[i].Constituency == Constituency_ID){
+      console.log(populations[i].Constituency)
+      populations2[Constituency_ID] += populations[i].AllAges 
+    }
+  }
+  console.log(populations2)
+  return populations2
 }
