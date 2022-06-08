f54b809baf116ea8a61654c5bba0c52571d9c0a1
haroldTheDeveloper <samuelb0801@gmail.com>
2022-06-08 18:28:43 +0100

    Made map rendering a function, testing multiple councils

--- map.php
+++ map.php
@@ -21,65 +21,15 @@
           <div id="map"></div>
           <script src="/scripts/mapEvents.js"></script>
           <script>
-            var councilDirectory = "/assets/outputAreas/E06000001.json"
+            var councilDirectory = ["/assets/outputAreas/E06000001.json", "/assets/outputAreas/E06000002.json", "/assets/outputAreas/E06000003.json", "/assets/outputAreas/E06000004.json"]
             let map = L.map("map", {center: [55.262218, -2.801472], zoom: 10});
             L.tileLayer(
                 "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
                 {attribution: "&copy; OpenStreetMap"}
             ).addTo(map);
-            fetch(councilDirectory)
-              .then(function(response) {
-                  return response.json();
-              })
-              .then(function(data) {
-                  L.geoJSON(data).on('click', function(e){
-                      var District_Feature = e.sourceTarget.feature;
-                      var layer = e.sourceTarget;
-                      console.log(layer)
-                      var District_ID = District_Feature.properties.OA11CD
-                      console.log(District_ID)
-                      fetch("/assets/Populations/Populations.json")
-                        .then(function(response) {
-                            return response.json();
-                        })
-                        .then(function(data) {
-                          var countOnClick = null
-                          var Constituency_ID = 1
-                          if (countOnClick == null){
-                            var newData = data
-                            newData = assign(District_ID, Constituency_ID, newData)
-                          }
-                          if (countOnClick == null){
-                            countOnClick = 1
-                          }
-                          if (populations2 == null){
-                            var colours = ['red', 'blue']
-                            var populations2 = [0,0]
-                            for (let i = 0; i <= colours; i++){
-                              populations2[i] == 0
-                            }
-                          }
-                          newData = assign(District_ID, Constituency_ID, newData)
-                          var populationID = populationDis(newData, District_ID)
-                          colourSeat(layer, Constituency_ID, colours)
-                          var populations2 = populationCalc(Constituency_ID, newData, populations2)
-                        })
-                      }).on('contextmenu', function(ev) {
-                        return false;
-                        Constituency_ID = 0
-                        fetch("/assets/Populations/Populations.json")
-                          .then(function(response) {
-                              return response.json();
-                          })
-                          .then(function(data){
-                            var data = assign(District_ID, Constituency_ID, data)
-                            colourSeat(layer, Constituency_ID, colours)
-                            var populations2 = populationCalc(Constituency_ID, data, populations2)
-                          })
-                      }, false)
-                  .addTo(map);
-              });
-
+            for (let i = 0; i < councilDirectory.length; i++){
+              renderMap(map, councilDirectory[i])
+            }
           </script>
         </section>
         <aside>
--- scripts/mapEvents.js
+++ scripts/mapEvents.js
@@ -1,4 +1,4 @@
-const assign = function(District_ID, Constituency_ID, populations){
+const assign2 = function(District_ID, Constituency_ID, populations){
   for (let i = 0; i < populations.length; i++){
     if (District_ID == populations[i].ID){
       populations[i].Constituency = Constituency_ID
@@ -34,3 +34,58 @@ const populationCalc = async function(Constituency_ID, populations, populations2
   console.log(populations2)
   return populations2
 }
+
+const renderMap = async function(map, councilDirectory){
+  fetch(councilDirectory)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        L.geoJSON(data).on('click', function(e){
+            var District_Feature = e.sourceTarget.feature;
+            var layer = e.sourceTarget;
+            console.log(layer)
+            var District_ID = District_Feature.properties.OA11CD
+            console.log(District_ID)
+            fetch("/assets/Populations/Populations.json")
+              .then(function(response) {
+                  return response.json();
+              })
+              .then(function(data) {
+                var countOnClick = null
+                var Constituency_ID = 1
+                if (countOnClick == null){
+                  var newData = data
+                  newData = assign2(District_ID, Constituency_ID, newData)
+                }
+                if (countOnClick == null){
+                  countOnClick = 1
+                }
+                if (populations2 == null){
+                  var colours = ['red', 'blue']
+                  var populations2 = [0,0]
+                  for (let i = 0; i <= colours; i++){
+                    populations2[i] == 0
+                  }
+                }
+                newData = assign2(District_ID, Constituency_ID, newData)
+                var populationID = populationDis(newData, District_ID)
+                colourSeat(layer, Constituency_ID, colours)
+                var populations2 = populationCalc(Constituency_ID, newData, populations2)
+              })
+            }).on('contextmenu', function(ev) {
+              return false;
+              Constituency_ID = 0
+              fetch("/assets/Populations/Populations.json")
+                .then(function(response) {
+                    return response.json();
+                })
+                .then(function(data){
+                  var data = assign2(District_ID, Constituency_ID, data)
+                  colourSeat(layer, Constituency_ID, colours)
+                  var populations2 = populationCalc(Constituency_ID, data, populations2)
+                })
+            }, false)
+        .addTo(map);
+    })
+  }
